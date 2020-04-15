b66a2bd9471a7356e1fcd25a29fdf2c6ba4ceb8c
Justinas Ba <Justinas-Ba@users.noreply.github.com>
2020-04-15 16:30:51 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+  
+from setuptools import setup
+
+setup(name = 'mysqlbackups',
+    version = '1',
+    description = 'Make mysql backup',
+    author = 'Justinas',
+    author_email = 'justas147@gmail.com',
+    url = 'https://github.com/Justinas-Ba/mysqlBackup.git',
+    packages = ['mysqlbackup'],
+    install_requires = [
+        'boto3',
+        'cryptography'],
+)
